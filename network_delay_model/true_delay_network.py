382e60b8b11f87ceff5bd059444703313d9de236
Ayush Khatiwada <ayush7khatiwada@gmail.com>
2025-02-23 21:35:57 +0000

    idk what I'm doing

--- network_delay_model/true_delay_network.py
+++ network_delay_model/true_delay_network.py
@@ -2,6 +2,9 @@ import networkx as nx
 import random
 from numpy.random import gamma
 
+from edges_with_gamma_params import edges_with_gamma_params
+from edges_with_normal_params import edges_with_normal_params
+
 
 class TrueDelayNetwork:
     def __init__(self):
@@ -9,18 +12,6 @@ class TrueDelayNetwork:
         self.SOURCE = 1
         self.DESTINATION = 5
         self.graph.add_nodes_from(range(1, 6))
-
-        edges_with_gamma_params = [
-            (1, 2, {"shape": 5.0, "scale": 2.0}),   # mean = 10.0
-            (2, 3, {"shape": 6.0, "scale": 1.5}),   # mean = 9.0
-            (3, 4, {"shape": 4.0, "scale": 2.5}),   # mean = 10.0
-            (4, 5, {"shape": 5.0, "scale": 2.0}),   # mean = 10.0
-
-            # Shortcut edges with slightly different characteristics
-            (1, 3, {"shape": 3.0, "scale": 3.5}),   # mean = 10.5
-            (2, 4, {"shape": 3.5, "scale": 3.0}),   # mean = 10.5
-            (3, 5, {"shape": 4.0, "scale": 2.0}),   # mean = 8.0
-        ]
 
         # use gamma distribution for delay at each edge
         self.is_distribution_gamma = True
